c5dc76584037f324cd275376ecedee36dcef89af
layal <layalhazeem@gmail.com>
2026-05-02 05:56:43 +0300

    layal last edits

--- lib/core/di/service_locator.dart
+++ lib/core/di/service_locator.dart
@@ -44,7 +44,7 @@ Future<void> setupServiceLocator() async {
 
   getIt.registerFactory<CompleteProfileCubit>(() => CompleteProfileCubit(getIt<AuthRepository>()));
 
-}
+
   getIt.registerFactory<CompleteProfileCubit>(
     () => CompleteProfileCubit(getIt<AuthRepository>()),
   );
